6ed579614a47b895b279030ce09153bcc54915e9
Hasinjaka <andrianomehasinjaka@gmail.com>
2024-10-09 22:22:01 +0300

    add_from_list_stock

--- src/app/components/stock/stock-list/stock-list.component.ts
+++ src/app/components/stock/stock-list/stock-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StockService } from '../../../services/stocks/stock.service';
 import { Stock } from '../../../models/stocks/stock';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stock-list',
@@ -10,11 +11,21 @@ import { Stock } from '../../../models/stocks/stock';
 export class StockListComponent implements OnInit {
   stocks: Stock[] = [];
 
-  constructor(private stockService: StockService) { }
+  constructor(private stockService: StockService, private router: Router) { }
 
   ngOnInit(): void {
     this.stockService.getStocks().subscribe(data => {
       this.stocks = data;
+    });
+  }
+
+  addStock(stock: Stock): void {
+    this.router.navigate(['/insert_stock'], {
+      queryParams: {
+        produit: stock.produit?.id,
+        prix_achat: stock.prix_achat_dep,
+        prix_vente: stock.prix_vente
+      }
     });
   }
 }
